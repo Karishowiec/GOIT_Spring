5d4c0b94b23f718972dfd19be152ac019fe6c11e
Karishowiec <karishowiec32@gmail.com>
2024-12-01 11:30:57 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/note/NoteService.java
@@ -0,0 +1,52 @@
+package com.example.demo.note;
+
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class NoteService {
+    private int id = 0;
+    private final Map<Integer, Note> notes = new HashMap<>();
+
+    private int incrementID() {
+
+        return id++;
+    }
+
+    public List<Note> listAll() {
+        return notes.values().stream().toList();
+    }
+
+    public Note add(Note note) {
+        note.setId(incrementID());
+        notes.put(note.getId(), note);
+        return note;
+    }
+
+    public void deleteById(int id) {
+        if (!notes.containsKey(id)) {
+            throw new IllegalArgumentException("note with id = " + id + " not founded");
+        }
+        notes.remove(id);
+    }
+
+    public void update(Note note) {
+        if (!notes.containsKey(note.getId())) {
+            throw new IllegalArgumentException("note with id = " + note.getId() + " not founded");
+        }
+        Note savedNote = notes.get(note.getId());
+        savedNote.setTitle(note.getTitle());
+        savedNote.setContent(note.getContent());
+    }
+
+    public Note getById(int id) {
+        if (!notes.containsKey(id)) {
+            throw new IllegalArgumentException("note with id = " + id + " not founded");
+        }
+        return notes.get(id);
+    }
+
+}
---
+++ src/test/java/com/example/demo/note/NoteServiceTest.java
@@ -0,0 +1,64 @@
+package com.example.demo.note;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NoteServiceTest {
+    NoteService noteService;
+    @BeforeEach
+    void setUp() {
+        noteService = new NoteService();
+    }
+
+    @Test
+    void listAll() {
+        noteService.add(new Note("please help", "no"));
+        noteService.add(new Note("please help", "no"));
+        noteService.add(new Note("please help", "no"));
+        noteService.add(new Note("please help", "no"));
+        noteService.add(new Note("please help", "no"));
+        assertEquals(5, noteService.listAll().size());
+    }
+
+    @Test
+    void add() {
+        Note note = new Note("please help", "no");
+        Note add = noteService.add(note);
+        assertNotNull(add);
+        assertEquals(note.getTitle(), add.getTitle());
+        assertEquals(note.getContent(), add.getContent());
+
+    }
+
+    @Test
+    void deleteById() {
+        Note note = new Note("please help", "no");
+        Note add = noteService.add(note);
+        noteService.deleteById(add.getId());
+        assertEquals(0,noteService.listAll().size());
+    }
+
+    @Test
+    void update() {
+        Note note = new Note("please help", "no");
+        Note add = noteService.add(note);
+        Note updated = new Note("lol", "kek", add.getId());
+        noteService.update(updated);
+        add = noteService.getById(add.getId());
+        assertNotNull(add);
+        assertEquals(updated.getTitle(), add.getTitle());
+        assertEquals(updated.getContent(), add.getContent());
+    }
+
+    @Test
+    void getById() {
+        Note note = new Note("please help", "no");
+        Note add = noteService.add(note);
+        add = noteService.getById(add.getId());
+        assertNotNull(add);
+        assertEquals(note.getTitle(), add.getTitle());
+        assertEquals(note.getContent(), add.getContent());
+    }
+}

87acff302e540fdfcd47e6e889a045ea721c5776
Karishowiec <karishowiec32@gmail.com>
2024-12-25 09:18:02 +0100

    add SecurityConfig

---
+++ src/main/java/com/example/demo/note/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.example.demo.note;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    private final DataSource dataSource;
+
+    public SecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/public/**").permitAll() // Дозволити доступ до ресурсів /public/** без авторизації
+                        .requestMatchers("/error").permitAll()     // Дозволити доступ до сторінки помилок
+                       .anyRequest().permitAll()             // Усі інші ресурси вимагають авторизації
+                )
+                .formLogin(withDefaults())                    // Увімкнути сторінку логіну за замовчуванням
+                .httpBasic(withDefaults());
+        http.cors(cors -> cors.disable());
+        http.csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("SELECT username, password, enabled FROM users WHERE username = ?")
+                .authoritiesByUsernameQuery("SELECT username, authority FROM authorities WHERE username = ?");
+    }
+}
